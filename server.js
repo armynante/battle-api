37eb3400b5543c0be0adba1d65e9d5f0893141d7
Andrew Armenante <andrew.armenante@gmail.com>
2015-10-01 15:40:48 -0400

    change

--- server.js
+++ server.js
@@ -1,60 +1,53 @@
-// var express    = require('express');        // call express
-// var app        = express();                 // define our app using express
-// var bodyParser = require('body-parser');
-// var mongoose   = require('mongoose');
-//
-// //User Models
-// var User       = require('./models/user');
-// mongoose.connect('mongodb://10.132.126.169/battle-api');
-// // mongoose.connect('mongodb://localhost/battle-api');
-//
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-//
-// var port = '8080';
-// var private_ip = '10.132.125.55';
-//
-// // ROUTES FOR OUR API
-// // =============================================================================
-// var router = express.Router();              // get an instance of the express Router
-//
-// router.get('/', function(req, res, next) {
-//   console.log('hitting home')
-//
-//     res.end("Hello, World!!!");
-// });
-//
-// router.route('/users')
-//     .post(function(req, res) {
-//         var user = new User();
-//         user.name = req.body.name;
-//
-//         user.save(function(err) {
-//             if (err) res.send(err);
-//
-//             res.json({ message: 'User created!' });
-//         })
-//     })
-//     .get(function(req,res) {
-//         User.find(function(err,users) {
-//           if(err) req.send(err);
-//           res.send(users);
-//         })
-//       });
-//
-//
-// // REGISTER OUR ROUTES -------------------------------
-// // all of our routes will be prefixed with /api
-// app.use(router);
-//
-// // START THE SERVER
-// // =============================================================================
-// app.listen(port,private_ip);
-// console.log('Server started on port ' + port);
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, '10.132.125.55');
-console.log('Server running at http://10.132.125.55:8080/');
+//User Models
+var User       = require('./models/user');
+mongoose.connect('mongodb://10.132.126.169/battle-api');
+// mongoose.connect('mongodb://localhost/battle-api');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = '8080';
+var private_ip = '10.132.125.55';
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res, next) {
+  console.log('hitting home')
+
+    res.end("Hello, World!!!");
+});
+
+router.route('/users')
+    .post(function(req, res) {
+        var user = new User();
+        user.name = req.body.name;
+
+        user.save(function(err) {
+            if (err) res.send(err);
+
+            res.json({ message: 'User created!' });
+        })
+    })
+    .get(function(req,res) {
+        User.find(function(err,users) {
+          if(err) req.send(err);
+          res.send(users);
+        })
+      });
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use(router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port,private_ip);
+console.log('Server started on port ' + port);
