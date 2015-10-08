6f3a36085afa42964f8fa796550b865132b93f07
Andrew Armenante <andrew.armenante@gmail.com>
2015-10-08 13:11:31 -0400

    isomorphic rendering of css transitions!

--- public/views/index.jsx
+++ public/views/index.jsx
@@ -1,13 +1,29 @@
 var React = require('react');
-var Page = require('./page.jsx')
+var Page = require('./page.jsx');
+
 
 var Index = React.createClass({
-  render: function() {
+  getInitialState: function() {
+    return {windowHeight: null, window: null};
+  },
+  handleResize: function(e) {
+      this.setState({windowHeight: e.srcElement.body.scrollTop});
+  },
+  componentDidMount: function() {
+    window.addEventListener('scroll', this.handleResize);
+  },
+  componentWillUnmount: function() {
+    window.removeEventListener('scroll', this.handleResize);
+  },
+  render:function(){
     return (
-      <page {...this.props}>
-        <h1>{this.props.title}</h1>
-      </page>
-    )
+        <Page {...this.props}>
+          <div className="headerStyle" >
+            <h1 className="logoText">Battle-API</h1>
+              <img src="images/battleship.png" className="ship" style={{right:this.state.windowHeight + 'px'}} />
+          </div>
+        </Page>
+      )
   }
 });
 
--- public/views/page.jsx
+++ public/views/page.jsx
@@ -1,5 +1,8 @@
 var React = require('react');
 
+var bodyStyle = {
+  height: '1200px'
+};
 
 var Page = React.createClass({
   render:function(){
@@ -8,8 +11,11 @@ var Page = React.createClass({
         <head>
           <title>{this.props.title}</title>
         </head>
-        <body>
+        <body style={bodyStyle}>
           {this.props.children}
+          <script src="/bundle.js"></script>
+          <link rel="stylesheet" type="text/css" href="/styles.css"></link>
+          <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'></link>
         </body>
       </html>
     )
